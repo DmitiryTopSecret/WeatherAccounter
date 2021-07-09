55ddf84de45831b654c6ff6a7c0243cb5774b7ad
dryabchenko <dizimka2255@mail.ru>
2021-07-09 18:12:09 +0300

    gitignore some bin folders

---
+++ WeatherAccounter.DAL/WeatherAccounterContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherAccounter.DAL
+{
+    public class WeatherAccounterContext : DbContext
+    {
+        public WeatherAccounterContext(DbContextOptions<WeatherAccounterContext> options)
+            :base(options)
+        {
+        }
+
+
+    }
+}
