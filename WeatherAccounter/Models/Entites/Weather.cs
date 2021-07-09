fc6c0d88e4651ddd0eda85f2bf60cf42ac93132b
dryabchenko <dizimka2255@mail.ru>
2021-07-09 16:45:34 +0300

    Added basic models

---
+++ WeatherAccounter/Models/Entites/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAccounter.Models.Entites
+{
+    public class Weather
+    {
+        public int WeatherId { get; set; }
+        public City City { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+        public int TemperatureC { get; set; }
+        public int MinTemperature { get; set; }
+        public int MaxTemperature { get; set; }
+        public int AvarageTemperature => (int)(MinTemperature + MaxTemperature) / 2;
+
+    }
+}
